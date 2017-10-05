53cc4ee7024b15bf7d6a57956a736145fffa0509
Che-Lin Huang <tiesto1114@gmail.com>
2017-10-05 18:15:36 -0500

    Initial Android Project

---
+++ app/src/main/java/team49/comfortfly/MainActivity.java
@@ -0,0 +1,13 @@
+package team49.comfortfly;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
