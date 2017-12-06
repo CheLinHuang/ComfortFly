dfe0f3c14ced6e46b607790c4aeded9528b187c9
Che-Lin Huang <tiesto1114@gmail.com>
2017-12-06 01:25:26 -0600

    remove edit password

--- app/src/main/java/team49/comfortfly/Setting.java
+++ app/src/main/java/team49/comfortfly/Setting.java
@@ -30,62 +30,21 @@ public class Setting extends AppCompatActivity {
     private static final int CAMERA_REQUEST = 1888;
     TextView textView_setting;
     EditText editNickName;
-    EditText editText_oldPassword;
-    EditText edit_password;
-    EditText edit_password_confirm;
     Button newNameBtn;
-    Button newPasswordBtn;
-    Button logout_btn;
     ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
-
         textView_setting = (TextView) findViewById(R.id.textView_setting);
-
         editNickName = (EditText) findViewById(R.id.editNickName);
-        editText_oldPassword = (EditText) findViewById(R.id.editText_oldPassword);
-        edit_password = (EditText) findViewById(R.id.edit_password);
-        edit_password_confirm = (EditText) findViewById(R.id.edit_password_confirm);
-
         newNameBtn = (Button) findViewById(R.id.newNameBtn);
         newNameBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 newNameBtn.setEnabled(false);
                 new editNickNameTask().execute(editNickName.getText().toString());
-            }
-        });
-
-        newPasswordBtn = (Button) findViewById(R.id.newPasswordBtn);
-        newPasswordBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (!checkPasswordMatch()) {
-                    edit_password_confirm.setError("The password doesn't match!");
-                    edit_password_confirm.requestFocus();
-                } else if (!isPasswordValid(edit_password.getText().toString())) {
-                    //edit_password.setError(getString(teamhardcoder.y_fi.R.string.error_invalid_password));
-                } else {
-                    newPasswordBtn.setEnabled(false);
-                    String[] passwords = new String[2];
-                    passwords[0] = editText_oldPassword.getText().toString();
-                    passwords[1] = edit_password.getText().toString();
-                    //new editPasswordTask(getApplicationContext()).execute(passwords);
-                }
-            }
-        });
-
-        logout_btn = (Button) findViewById(R.id.logout_btn);
-        logout_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                logout_btn.setEnabled(false);
-                /*Intent intent = new Intent(Settings.this, Login.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(intent);*/
             }
         });
 
@@ -108,14 +67,6 @@ public class Setting extends AppCompatActivity {
         }
     }
 
-    private boolean checkPasswordMatch() {
-        return edit_password.getText().toString().equals(edit_password_confirm.getText().toString());
-    }
-
-    private boolean isPasswordValid(String password) {
-        return password.length() > 4;
-    }
-
     class getImageTask extends AsyncTask<Void, Void, Void> {
 
         Bitmap bitmap;
@@ -123,7 +74,7 @@ public class Setting extends AppCompatActivity {
         @Override
         protected Void doInBackground(Void... params) {
             try {
-                bitmap = BitmapFactory.decodeStream((InputStream) new URL("http://e1.mail.thecheesecakefactory.com/files/amf_cheesecake_factory/project_1/17-12-04_Day_of_10000_Slices_Teaser/promo1.jpg").getContent());
+                bitmap = BitmapFactory.decodeStream((InputStream) new URL("https://ws.engr.illinois.edu/directory/viewphoto.aspx?id=4960&s=215&type=portrait").getContent());
 
                 ByteArrayOutputStream bao = new ByteArrayOutputStream();
                 bitmap.compress(Bitmap.CompressFormat.JPEG, 90, bao);
@@ -152,8 +103,8 @@ public class Setting extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(Void result) {
-            System.out.println(bitmap.toString());
-            imageView.setImageBitmap(bitmap);
+            if (bitmap != null)
+                imageView.setImageBitmap(bitmap);
         }
     }
 
@@ -182,38 +133,8 @@ public class Setting extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(final Boolean success) {
+            editNickName.setText("");
             newNameBtn.setEnabled(true);
         }
     }
-
-
-    class editPasswordNameTask extends AsyncTask<String, Void, Boolean> {
-
-        @Override
-        protected Boolean doInBackground(String... params) {
-            HttpClient httpclient = new DefaultHttpClient();
-            HttpPost httppost = new HttpPost("http://fa17-cs411-49.cs.illinois.edu/api/account?token=" + Home.token);
-            try {
-                httppost.setEntity(new StringEntity("{\"action\":\"edit\",\"name\":\"" + params[0] + "\"}"));
-                HttpResponse response = httpclient.execute(httppost);
-                System.out.println(response.toString());
-
-                System.out.println(response.getStatusLine().getStatusCode());
-                if (response.getStatusLine().getStatusCode() == 200) {
-                    String responseString = EntityUtils.toString(response.getEntity());
-                    System.out.println(responseString);
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-                return false;
-            }
-            return true;
-        }
-
-        @Override
-        protected void onPostExecute(final Boolean success) {
-            newNameBtn.setEnabled(true);
-        }
-    }
-
 }
